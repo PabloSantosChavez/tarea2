dc268bf08888b455867e9042aa02ef2d4ca87ba0
Pablo <pablosantoschavez2001@gmail.com>
2024-09-10 23:56:48 -0300

    commit2

--- Operadores_Aritmeticos.js
+++ Operadores_Aritmeticos.js
@@ -15,6 +15,8 @@ Se debe hacer lo siguiente:
 5. `Modulo (%)`:  Saca el modulo entre numero2 y numero1. Muestra el resultado por consola.
 */
 
+const numero1 = 20;
+const numero2 = 40;
 
 //1. `Suma (+)`: Suma numero1 y numero2. Muestra el resultado por consola.
 const Suma = numero1 + numero2;
